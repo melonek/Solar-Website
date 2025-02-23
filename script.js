f221742fdc26d4edb19d6c4e05af7f7e9ce05cd3
melonek <meloniusz@hotmail.com>
2025-02-24 06:40:21 +0800

    .

--- script.js
+++ script.js
@@ -56,39 +56,37 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   
   // -------------------------
-  // FACEBOOK TIMELINES PRELOAD & SDK INITIALIZATION (v16.0)
-  // -------------------------
-  window.fbAsyncInit = function() {
-    FB.init({
-      xfbml   : true,
-      version : 'v16.0'
-    });
-    const wrappers = document.querySelectorAll('.fb-page-wrapper');
-    wrappers.forEach(wrapper => {
-      if (!wrapper.classList.contains('fb-parsed')) {
-        FB.XFBML.parse(wrapper);
-        wrapper.classList.add('fb-parsed');
-      }
-    });
-  };
-  
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-  
-  function preloadFBTimelines() {
-    const wrappers = document.querySelectorAll('.fb-page-wrapper');
-    wrappers.forEach(wrapper => {
+// FACEBOOK TIMELINES PRELOAD & SDK INITIALIZATION (v16.0)
+// -------------------------
+window.fbAsyncInit = function() {
+  FB.init({
+      appId     : '956735296637387',  // Replace with your Facebook App ID
+      xfbml     : true,
+      version   : 'v16.0'
+  });
+
+  // After FB initialization, parse the timelines
+  preloadFBTimelines();
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function preloadFBTimelines() {
+  const wrappers = document.querySelectorAll('.fb-page-wrapper');
+  wrappers.forEach(wrapper => {
       if (typeof FB !== 'undefined' && !wrapper.classList.contains('fb-parsed')) {
-        FB.XFBML.parse(wrapper);
-        wrapper.classList.add('fb-parsed');
+          FB.XFBML.parse(wrapper);
+          wrapper.classList.add('fb-parsed');
       }
-    });
-  }
+  });
+}
+
   
   // -------------------------
   // REVEAL FUNCTIONS
