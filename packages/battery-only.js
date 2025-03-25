03c5227c3ea6a910e78d529f5e62d69d20165167
melonek <meloniusz@hotmail.com>
2025-03-25 08:03:02 +0800

    .

--- packages/battery-only.js
+++ packages/battery-only.js
@@ -1,16 +1,3 @@
-// Service Worker Registration
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
-
 (function () {
   // Scoped variables
   let hasShownBatteryCloud = false;
